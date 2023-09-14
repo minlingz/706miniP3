3155c065922363802a52f0d3e1bb1859bb80f3fb
minlingz <minling.zhou@gmail.com>
2023-09-13 21:11:25 -0400

    update code with polars

--- ds.py
+++ ds.py
@@ -1,18 +1,16 @@
-"""Python script to read csv file, 
-    generate statistics summary 
-        and plot interested columns """
-#import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import polars as pl
+import plotly.express as px
 
-# Read csv file "world-small.csv"
-data = pd.read_csv("world-small.csv")
-# convert "gdppcap08" column to integer
-data["gdppcap08"] = data["gdppcap08"].astype("int")
-# print out statistics summary of "gdppcap08" & "polityIV"
-print(data[["gdppcap08", "polityIV"]].describe())
+# Read csv file "world-small.csv" using Polars
+data = pl.read_csv("world-small.csv")
 
-# scatter plot of "gdpcap08" as x & "polityIV" as y
-data.plot.scatter(x="gdppcap08", y="polityIV", alpha=0.5)
-plt.show()
-plt.savefig('plot.png')
+# Convert "gdppcap08" column to integer
+data = data.with_columns(data["gdppcap08"].cast(pl.Int32))
+
+# Print out statistics summary of "gdppcap08" & "polityIV"
+summary = data.select(["gdppcap08", "polityIV"]).describe()
+print(summary)
+
+fig = px.scatter(data, x="gdppcap08", y="polityIV")
+fig.show()
+fig.write_image("plot.png")
